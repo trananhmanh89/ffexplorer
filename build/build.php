2d1c1d0cbf337c23a63ba406110035cfecc4dfda
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2020-02-13 15:34:00 +0700

    add build script

---
+++ build/build.php
@@ -0,0 +1,52 @@
+<?php 
+use Joomla\Filesystem\Folder;
+use Joomla\Archive\Zip;
+
+define('JPATH_ROOT', realpath(__DIR__ . '/..') );
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+$path = realpath(JPATH_ROOT . '/administrator/components/com_ffexplorer/');
+
+$excludeFolders = array('.svn', 'CVS', '.DS_Store', '__MACOSX', 'node_modules');
+$files = Folder::files($path, '.', true, true, $excludeFolders);
+$contents = array();
+
+foreach ($files as &$file) {
+    $info = pathinfo($file);
+    if ($info['basename'] === 'ffexplorer.xml') {
+        continue;
+    }
+
+    $content = array();
+    $content['data'] = file_get_contents($file);
+
+    $name = realpath($file);
+    $name = 'admin' . str_replace($path, '', $name);
+    $content['name'] = $name;
+    $contents[] = $content;
+}
+
+$xmlContent = file_get_contents($path . '/ffexplorer.xml');
+$contents[] = array(
+    'name' => 'ffexplorer.xml',
+    'data' => $xmlContent
+);
+
+$languages = array(
+    'en-GB.com_ffexplorer.ini',
+    'en-GB.com_ffexplorer.sys.ini'
+);
+
+foreach ($languages as $lang) {
+    $contents[] = array(
+        'name' => 'admin/language/' . $lang,
+        'data' => JPATH_ROOT . '/administrator/language/en-GB/' . $lang
+    );
+}
+
+$archive = new Zip();
+$archive->create(JPATH_ROOT . '/ffexplorer.zip', $contents);
+
+echo "\n===> build done <===\n";
