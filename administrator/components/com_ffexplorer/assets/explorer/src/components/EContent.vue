c422076e22e56cbdb85af97ed0d984bb145dfe6c
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2020-08-06 17:13:26 +0700

    remove middle mouse event

--- administrator/components/com_ffexplorer/assets/explorer/src/components/EContent.vue
+++ administrator/components/com_ffexplorer/assets/explorer/src/components/EContent.vue
@@ -12,7 +12,6 @@
                 :class="{active: file.path === current, deleted: file.status === 'deleted'}"
                 :title="file.path"
                 @click="open(file)"
-                @mouseup.middle="close(file.path)"
             >
                 <div class="file-status is-saving" v-if="file.status === 'saving'">•</div>
                 <div class="file-status is-dirty" v-if="file.status === 'dirty'">•</div>
@@ -28,7 +27,7 @@
                     license="https://fontawesome.com/license" 
                     viewBox="0 0 352 512"
                     :style="{visibility: file.path === current ? 'visible' : 'hidden'}" 
-                    @click.stop="close(file.path)"
+                    @click.prevent.stop="close(file.path)"
                 >
                     <path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
                 </svg>
